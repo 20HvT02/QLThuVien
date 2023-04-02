8b01680d23c541f3d16d4d64112679c6ec452159
TranXuanTien <tranxuantien192@gmail.com>
2023-04-02 20:36:50 +0700

    Add DanhMucSach for index of HomeAdmin

---
+++ QLThuVien/Areas/Admin/Controllers/HomeAdminController.cs
@@ -0,0 +1,35 @@
+﻿using Azure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QLThuVien.Models;
+using X.PagedList;
+
+namespace QLThuVien.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("admin")]
+    [Route("admin/homeadmin")]
+    public class HomeAdminController : Controller
+    {
+        QlthuVienLtwebContext db = new QlthuVienLtwebContext();
+
+        [Route("")]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("DanhMucSach")]
+        public IActionResult DanhMucSach(int? page)
+        {
+            int pageNumber = page == null || page < 1 ? 1 : page.Value;
+            int pageSize = 12;
+
+            var lstsanpham = db.Saches.AsNoTracking().OrderBy(x => x.TenSach);
+
+            PagedList<Sach> lst = new PagedList<Sach>(lstsanpham, pageNumber, pageSize);
+            return View(lst);
+        }
+    }
+}
