4fbb4e1683d993301ce7e51ac05065c81394d1e4
TranXuanTien <tranxuantien192@gmail.com>
2023-04-02 19:00:22 +0700

    (add data models) nhớ sửa cái models này nhá

---
+++ QLThuVien/Models/PhieuMuon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QLThuVien.Models;
+
+public partial class PhieuMuon
+{
+    public string MaHoSoMuon { get; set; } = null!;
+
+    public DateTime? NgayMuon { get; set; }
+
+    public DateTime? ThơiGianCho { get; set; }
+
+    public string? TinhTrang { get; set; }
+
+    public string MaHoSotra { get; set; } = null!;
+
+    public string MaNhanVien { get; set; } = null!;
+
+    public string MaNguoiDoc { get; set; } = null!;
+
+    public virtual ICollection<ChiTietHsm> ChiTietHsms { get; } = new List<ChiTietHsm>();
+
+    public virtual HoSoTra MaHoSotraNavigation { get; set; } = null!;
+
+    public virtual NguoiDoc MaNguoiDocNavigation { get; set; } = null!;
+
+    public virtual NhanVien MaNhanVienNavigation { get; set; } = null!;
+}
